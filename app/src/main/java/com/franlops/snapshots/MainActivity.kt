6185d484a15a4e6360a6d42e2d2cf362fedc8547
Francisco Lopez <franlopbri@gmail.com>
2021-11-24 13:14:59 -0600

    Authentication - Mejoras

--- app/src/main/java/com/franlops/snapshots/MainActivity.kt
+++ app/src/main/java/com/franlops/snapshots/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.franlops.snapshots
 
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 
 import com.franlops.snapshots.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -94,5 +97,18 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         mFirebaseAuth?.removeAuthStateListener(mAuthListener)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Bienvenido....", Toast.LENGTH_SHORT).show()
+            } else {
+                if(IdpResponse.fromResultIntent(data) == null){
+                    finish()
+                }
+            }
+        }
     }
 }
